346c68eaf3f9b403fba12bfcc1fae0724b4cab36
James Nicholson <nicholsonjb@vcu.edu>
2017-04-22 08:27:46 -0400

    Music Class added

--- INFO450Music/INFO450Music.cpp
+++ INFO450Music/INFO450Music.cpp
@@ -2,6 +2,25 @@
 //
 
 #include "stdafx.h"
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+const int READERROR = 100;
+const int WRITEERROR = 200;
+const int ARRAYSIZE = 100;
+
+class Music 
+{
+	string artist;
+	string song;
+	Music *next;
+
+public:
+
+};
 
 
 int main()
