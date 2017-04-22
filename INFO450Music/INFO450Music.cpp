8f2935be460dc316b40c2446d3aa1523e296f773
James Nicholson <nicholsonjb@vcu.edu>
2017-04-22 08:42:25 -0400

    LinkedList added

--- INFO450Music/INFO450Music.cpp
+++ INFO450Music/INFO450Music.cpp
@@ -12,16 +12,29 @@ const int READERROR = 100;
 const int WRITEERROR = 200;
 const int ARRAYSIZE = 100;
 
-class Music 
+class music 
 {
-	string artist;
-	string song;
-	Music *next;
+	string artistName;
+	string songName;
+	music *next;
 
 public:
-
+	music(string artist, string song);
+	friend class musickLinkedList;
 };
 
+
+class musicLinkedList
+{
+	music *head;
+	music *tail;
+public:
+	musicLinkedList();
+	void skipMusicNode(); //Skip a node
+	void removeMusicNode(); //Delete a node
+	void showMusicList(); //Transverse a list
+
+};
 
 int main()
 {
