976f2fff8cf814eb2dd99cc9f03ab690753ae84b
James Nicholson <nicholsonjb@vcu.edu>
2017-04-26 22:25:07 -0400

    Updating delete

--- INFO450Music/INFO450Music.cpp
+++ INFO450Music/INFO450Music.cpp
@@ -18,6 +18,7 @@ class music
 {
 	string songName;
 	string songArtist;
+	int p;
 	music* next;
 
 public:
@@ -91,32 +92,66 @@ void linkedList::showList()
 {
 	music* ptr = head;
 	music* prev = NULL;
+	
 
 	string choice;
 
 	int amount;
-
+	
 
 	cout << "**** My Music List **** " << endl;
 	if (head == NULL)
 	{
 		return;
 	}
+
+	if (head->songName == choice)
+	{
+		//if only 1 node in the list
+		if (head == tail)
+		{
+			head = NULL;
+			tail = NULL;
+		}
+		else
+			head = head->next;
+		delete ptr;
+		return;
+	}
 	while (ptr != NULL)
 	{
-		
-		do
-		{
-			
-			cout << "The song is " << ptr->songName << " by " << ptr->songArtist << " [P]lay, [S]kip,[D]elete, or [Q]uit? ";
-			getline(cin, choice);
+		cout << "The song is " << ptr->songName << " by " << ptr->songArtist << " [P]lay, [S]kip,[D]elete, or [Q]uit? ";
+		getline(cin, choice);
 
-			if (choice == "p" || choice == "P")
+		if (choice == "p" || choice == "P")
+		{
+			cout << "---------\n";
+			cout << "Playing: ";
+			ptr->showSong();
+			cout << "---------\n";
+			if (ptr == tail)
 			{
-				cout << "---------\n";
-				cout << "Playing: ";
+				ptr = head;
+			}
+			else
+			{
+				ptr = ptr->next;
+			}
+		}
+		//skip song on list
+		if (choice == "s" || choice == "S")
+		{
+			cout << "How many?" << endl;
+			cin >> amount;
+			cin.clear();
+			cin.ignore();
+			cout << "---------\n";
+			for (int i = 1; ptr && i <= amount; i++)
+			{
+				cout << "Skipping: ";
 				ptr->showSong();
-				cout << "---------\n";
+
+				prev = ptr;
 				if (ptr == tail)
 				{
 					ptr = head;
@@ -125,136 +160,120 @@ void linkedList::showList()
 				{
 					ptr = ptr->next;
 				}
-				
 			}
-			//skip song on list
-			if (choice == "s" || choice == "S")
-			{
-				cout << "How many?" << endl;
-				cin >> amount;
-				cin.clear();
-				cin.ignore();
-				cout << "---------\n";
-				for (int i = 1; ptr && i <= amount; i++)
-				{
-					cout << "Skipping: ";
-					ptr->showSong();
+			cout << "---------\n";
+		}
 
-					prev = ptr;
-					if (ptr == tail)
-					{
-						ptr = head;
-					}
-					else
-					{
-						ptr = ptr->next;
-					}
-				}
-				cout << "---------\n";
+		//Delete song from list
+		if (choice == "D" || choice == "d")
+		{
+	
+
+			if (ptr->next && (ptr->next)->songName == amount)
+			{
+				if (tail == ptr->next)
+					tail = ptr;
+
+				music* tbd = ptr->next;
+				ptr->next = (ptr->next)->next;
+				delete tbd;
+				return;
 			}
-
-			//Delete song from list
-			if (choice == "D" || choice == "d")
+			cout << "---------\n";
+			cout << "Deleting: ";
+			ptr->showSong();
+			cout << "---------\n";
+			if (ptr == tail)
 			{
-				if (ptr->next && (ptr->next)->songName == choice)
-				{
-					if (tail == ptr->next)
-						tail = ptr;
-
-					music* tbd = ptr->next;
-					ptr->next = (ptr->next)->next;
-					delete tbd;
-					return;
-				}
-				cout << "---------\n";
-				cout << "Deleting: ";
-				ptr->showSong();
-				cout << "---------\n";
+				ptr = head;
+			}
+			else
+			{
 				ptr = ptr->next;
 			}
-			//Quit List
-			if (choice == "Q" || choice == "q")
-			{
-				cout << "Thanks for listening! " << endl;
-			}
-	
 		}
-		while (choice != "Q" && choice != "q");
-	}
-}
 
-void linkedList::addNodeToEnd(music* newnode)
-{
-	if (head == NULL)
-	{
-		head = newnode;
-		tail = newnode;
-	}
-	else
-	{
-		tail->next = newnode;
-		tail = newnode;
-	}
-}
-
-
-int linkedList::readList(string filename)
-{
-	string isong, iname;
-	ifstream infile(filename, ios::in);
-	if (!infile)
-	{
-		cout << "File could not be opened for reading" << endl;
-		return READERROR;
-	}
-
-
-	while (!infile.eof())
-	{
-		getline(infile, iname, '|');
-		if (!iname.empty())
+		//Quit List
+		if (choice == "Q" || choice == "q")
 		{
-			getline(infile, isong);
+			cout << "Thanks for listening! " << endl;
+			break;
 
-			music* newnode = new music(isong, iname);
-			addNodeToEnd(newnode);
-
-			numrecords++;
+		}
+	}
+}
+	void linkedList::addNodeToEnd(music* newnode)
+	{
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
 		}
 	}
 
-	infile.close();
-	return 0;
-}
 
-int main()
-{
-	linkedList my;
-
-	int error;
-	string answer;
-	string filename;
-
-	cout << "Welcome to  Music Player!" << endl;
-	cout << "To access the list text file. Use the file path where the text file" << endl;
-	cout << "is stored on your machine. Ex. C:\\Projects\\MyMusic.txt" << endl;
-
-	cout << "Enter the full path of the file: " << endl;
-	getline(cin, filename);
-	ifstream file(filename);
-	error = my.readList(filename);
-	if (error)
+	int linkedList::readList(string filename)
 	{
-		cout << "Cannot read list" << endl;
-		cout << "Possible issues:" << endl;
-		cout << "1. Incorrect file name" << endl;
-		cout << "2. File does not exist " << endl;
-		cout << "3. Incorrect file path" << endl;
+		string isong, iname;
+		ifstream infile(filename, ios::in);
+		if (!infile)
+		{
+			cout << "File could not be opened for reading" << endl;
+			return READERROR;
+		}
+
+
+		while (!infile.eof())
+		{
+			getline(infile, iname, '|');
+			if (!iname.empty())
+			{
+				getline(infile, isong);
+
+				music* newnode = new music(isong, iname);
+				addNodeToEnd(newnode);
+
+				numrecords++;
+			}
+		}
+
+		infile.close();
+		return 0;
+	}
+
+	int main()
+	{
+		linkedList my;
+
+		int error;
+		string answer;
+		string filename;
+
+		cout << "Welcome to  Music Player!" << endl;
+		cout << "To access the list text file. Use the file path where the text file" << endl;
+		cout << "is stored on your machine. Ex. C:\\Projects\\MyMusic.txt" << endl;
+
+		cout << "Enter the full path of the file: " << endl;
+		getline(cin, filename);
+		ifstream file(filename);
+		error = my.readList(filename);
+		if (error)
+		{
+			cout << "Cannot read list" << endl;
+			cout << "Possible issues:" << endl;
+			cout << "1. Incorrect file name" << endl;
+			cout << "2. File does not exist " << endl;
+			cout << "3. Incorrect file path" << endl;
+
+			return 0;
+		}
+		my.showList();
+
 
 		return 0;
 	}
-	my.showList();
-
-
-	return 0;
-}
